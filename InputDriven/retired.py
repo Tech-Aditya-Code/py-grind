0797d060efe338fcbe97a4905ffaf27757e21b9f
Aditya Mungekar <techaditya36@gmail.com>
2025-04-12 13:09:46 +0530

    Add files via upload

---
+++ InputDriven/retired.py
@@ -0,0 +1,13 @@
+# RetirementChecker
+
+def retirement():
+
+    age = int(input("Enter age: "))
+
+    if age >= 60:
+        print("you are retired")
+    else:
+        print("you are not retired")
+
+    retirement()
+retirement()
