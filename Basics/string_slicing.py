58c73ae72b4d08c2328e164c1b81a08ad0485c0f
Aditya Codes <techaditya36@gmail.com>
2025-05-11 22:18:26 +0530

    Add files via upload

---
+++ Basics/string_slicing.py
@@ -0,0 +1,15 @@
+w = "welcome to Wscubetech"
+
+# print(w[6])
+
+# print(w[-1])
+# print(w[-13])
+# print(w[8])
+
+# slicing
+
+print(w[0:7])
+print(w[0:])
+print(w[0::2])
+print(w[-1::-2])
+print(w[-1::-1])
